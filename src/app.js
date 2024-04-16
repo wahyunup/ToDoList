a9e7e0b9b6a5fbbf46d9647276b0cba8df3d4d53
wahyunup <wahyunurpambuko@gmail.com>
2024-04-16 13:41:44 +0700

    add test

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const todo = document.getElementById('todo');
 function add(){
     let newText = document.getElementById('input');
     let newTodo = `
-    <div  class="flex items-center justify-between">
+    <div id="remove" class="flex items-center justify-between">
     <span id="text" class="text-sm mr-10" >${newText.value}</span>
     <div class="flex gap-2">
         <button class="p-3 bg-slate-700 hover:bg-green-800 flex text-center items-center rounded-sm"><i class="fa-solid fa-check"></i></button>
-        <button id="remove" onclick="clear()" class="p-3 bg-slate-700 hover:bg-red-800 flex text-center items-center rounded-sm"><i class="fa-solid fa-trash"></i></button>
+        <button  onclick="clearItems()" class="p-3 bg-slate-700 hover:bg-red-800 active:bg-red-900 flex text-center items-center rounded-sm"><i class="fa-solid fa-trash"></i></button>
     </div>
 </div>`
     todo.insertAdjacentHTML("beforeend", newTodo)
@@ -17,7 +17,12 @@ function add(){
 }
 
 // menambahkan fungsi delete
-function clear(){
+function clearItems(){
     let removeItems = document.getElementById('remove')
     removeItems.parentNode.removeChild(removeItems);
+}
+
+//menambahkan fungsi coret
+function sliceItems(){
+
 }
