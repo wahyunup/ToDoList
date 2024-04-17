d7897822476a284017ec16a90a60617babeeecf8
wahyunup <wahyunurpambuko@gmail.com>
2024-04-17 10:26:46 +0700

    todo launch v1.0

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ function add(){
     let newText = document.getElementById('input');
     let newTodo = `
     <div id="remove" class="flex items-center justify-between">
-    <span id="text" class="text-sm mr-10" >${newText.value}</span>
+    <p id="slice" class="text-sm mr-10" >${newText.value}</p>
     <div class="flex gap-2">
-        <button class="p-3 bg-slate-700 hover:bg-green-800 flex text-center items-center rounded-sm"><i class="fa-solid fa-check"></i></button>
-        <button  onclick="clearItems()" class="p-3 bg-slate-700 hover:bg-red-800 active:bg-red-900 flex text-center items-center rounded-sm"><i class="fa-solid fa-trash"></i></button>
+        <button onclick="sliceItems()" class="p-3 bg-slate-700 hover:bg-green-800 flex text-center items-center rounded-sm"><i class="fa-solid fa-check"></i></button>
+        <button onclick="clearItems()" class="p-3 bg-slate-700 hover:bg-red-800 flex text-center items-center rounded-sm"><i class="fa-solid fa-trash"></i></button>
     </div>
 </div>`
     todo.insertAdjacentHTML("beforeend", newTodo)
@@ -24,5 +24,6 @@ function clearItems(){
 
 //menambahkan fungsi coret
 function sliceItems(){
-
+    let sliceingItems = document.getElementById('slice')
+    sliceingItems.style.textDecoration = "line-through";
 }
